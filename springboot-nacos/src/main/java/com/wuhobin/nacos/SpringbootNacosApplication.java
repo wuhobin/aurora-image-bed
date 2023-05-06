1b700d57a3259c1782e8284039fa85dc7fbf679d
wuhobin <1289066006@qq.com>
2023-05-06 18:21:14 +0800

    修改依赖

--- springboot-nacos/src/main/java/com/wuhobin/nacos/SpringbootNacosApplication.java
+++ springboot-nacos/src/main/java/com/wuhobin/nacos/SpringbootNacosApplication.java
@@ -1,13 +1,19 @@
 package com.wuhobin.nacos;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * @author wuhonbin
+ */
 @EnableCaching
+@ComponentScan(basePackages = "com.wuhobin")
 @SpringBootApplication
 @EnableDiscoveryClient
 public class SpringbootNacosApplication extends SpringBootServletInitializer {
